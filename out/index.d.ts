586e9dd11b0cd9bf6e2dd2ec398570d49328099f
SAC <sac.673@gmail.com>
2023-07-27 13:34:26 +0200

    gnar

---
+++ out/index.d.ts
@@ -0,0 +1,3 @@
+export declare const summary: (path: string, save_json: string) => object;
+declare const _default: (path: string | null, dom_obj: HTMLDivElement | null, save_json?: string) => void;
+export default _default;
---
+++ out/util.d.ts
@@ -0,0 +1,3 @@
+export declare function formatMs(ms: number, decimals?: number): string;
+export declare function formatBytes(bytes: number, decimals?: number): string;
+export declare function keyGen(len?: number): string;
